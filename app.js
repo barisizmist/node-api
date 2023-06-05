035e8fa51c158f716048f02a6536dfff38498c25
baris.tas <baris.tas@adesso.com.tr>
2023-06-05 18:06:16 +0300

    remove unnes comments

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// const http = require('http');
-// const hostname = '127.0.0.1';
-// const port = 4000;
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'application/json');
-//   const paylaod = {
-//     data: {
-//       success: true,
-//       created: true
-//     }
-//   };
-//   res.end(JSON.stringify(paylaod));
-// });
-// server.listen(port, hostname, () => {
-//   console.log(`Server listening at http://${hostname}:${port}`);
-// });
 const express = require('express');
 const app = express();
 const hostname = '127.0.0.1';
